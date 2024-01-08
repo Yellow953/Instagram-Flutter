b0809d66d22239793fc469dbcaff579e5ae17770
Yellow <joemazloum953@gmail.com>
2024-01-08 16:04:17 +0100

    setup firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,28 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram_flutter/responsive/mobile_screen_layout.dart';
 import 'package:instagram_flutter/responsive/responsive_layout_screen.dart';
 import 'package:instagram_flutter/responsive/web_screen_layout.dart';
 import 'package:instagram_flutter/utils/colors.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+        apiKey: 'AIzaSyAoL3C3ndRpYTW96_g-O6FKvwX2PtFxeaY',
+        appId: '1:254148920381:web:0f4aa2cc869f7a06e3d654',
+        messagingSenderId: '254148920381',
+        projectId: 'instagram-bf84d',
+        storageBucket: 'instagram-bf84d.appspot.com',
+      ),
+    );
+  } else {
+    await Firebase.initializeApp();
+  }
+
   runApp(const MyApp());
 }
 
